96e05cb933207fa83510e2e6e9015a21503b6d8a
Josue Arce <josuerc026@gmail.com>
2018-07-08 16:09:19 -0400

    register component and hooking it to register endpoint

--- kanban-client/src/components/register.vue
+++ kanban-client/src/components/register.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
         <h1>Register</h1>
+        <input type="text" placeholder="email" name="email" v-model="email">
+        <input type="password" name="password" placeholder="password" v-model="password">
+        <button @click="register">Register</button>
     </div>
 </template>
 
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
+
 export default {
-  name: 'register'
+  name: 'register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
 }
 </script>
 
